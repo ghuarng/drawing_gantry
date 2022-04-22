cfad931b9220299c854882de832c488507218e0c
ghuarng <gary0928@gmail.com>
2022-04-21 20:00:33 -0700

    Add full movement capabilities, serial S&W

---
+++ gantry_serial/gantry.h
@@ -0,0 +1,22 @@
+#define MOTOR_STEPS 200 //1.8 deg/step; Each rotation ~2cm
+
+//X Motor (+ RIGHT, - LEFT)
+#define X_DIR  3
+#define X_STEP 4
+
+//Y Motor (+ DOWN, - UP)
+#define Y_DIR  6
+#define Y_STEP 7
+
+//Servo 
+#define SERVO 2
+#define UP    135 //servo angles
+#define DOWN  146
+#define DONE  90
+
+void penDown();
+void penUp();
+void moveX();
+void moveY();
+void sendGantryToDraw();
+void sendGantryToOrigin();
---
+++ serial_test/serial_test.ino
@@ -0,0 +1,18 @@
+#include <Servo.h>
+
+#define SERVO 12
+Servo myServo;
+
+void setup() {
+  Serial.begin(9600);
+  myServo.attach(SERVO);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(Serial.available() > 0){
+    int msg = Serial.read();
+    myServo.write(msg);
+    Serial.println(msg);
+  }
+}
